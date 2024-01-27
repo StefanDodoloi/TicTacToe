dda7699634ef5a557be9402ec06b31c32413c981
StefanDodoloi <153545504+StefanDodoloi@users.noreply.github.com>
2024-01-27 20:23:29 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const cells = document.querySelectorAll('.cell');
+const gameOver = document.querySelector('#gameOver');
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+let options;
+let currentPlayer = 'X';
+
+newGame();
+
+function newGame() {
+    cells.forEach(cell => cell.addEventListener('click', cellClicked));
+    currentPlayer = 'X';
+    options = ['', '', '', '', '', '', '', '', ''];
+    cells.forEach(cell => cell.textContent = '');
+    gameOver.textContent = '';
+}
+
+function cellClicked() {
+    const cellIndex = this.getAttribute('cellIndex'); 
+    if (options[cellIndex] !== '' || gameOver.textContent !== '') {
+        return;
+    }
+    options[cellIndex] = currentPlayer;
+    this.textContent = currentPlayer;
+    checkWinner();
+}
+
+function checkWinner() {
+    let gameEnd = false;
+    for (let i = 0; i < winConditions.length; i++) {
+        const condition = winConditions[i];
+        const cellA = options[condition[0]];
+        const cellB = options[condition[1]];
+        const cellC = options[condition[2]];
+        if (cellA === '' || cellB === '' || cellC === '') {
+            continue;
+        }
+        if (cellA == cellB && cellB == cellC) {
+            gameEnd = true;
+            break;
+        }
+    }
+    if (gameEnd) {
+        gameOver.textContent = `${currentPlayer} wins!`;
+    } else if (!options.includes('')) {
+        gameOver.textContent = `Draw!`;
+    } else {
+        currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+    }
+}
+
